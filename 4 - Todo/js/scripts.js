ea1d0a494426f6638bcb81654f381fa2cfd18eab
Dcmaran <diogocmr2002@gmail.com>
2023-10-27 13:36:07 -0300

    .

--- 4 - Todo/js/scripts.js
+++ 4 - Todo/js/scripts.js
@@ -8,18 +8,146 @@ const searchInput = document.querySelector("#search-input");
 const eraseBtn = document.querySelector("#erase-button");
 const filterBtn = document.querySelector("#filter-select");
 
+let oldInputValue;
 
 const saveTodo = (text) => {
 
     const todo = document.createElement("div");
     todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+
+    todo.inputValue = "";
+    todoInput.focus();
+
+};
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
 }
+
+const getSearchTodos = (search) => {
+
+    const todos = document.querySelectorAll(".todo")
+    
+    todos.forEach((todo) => {
+
+        let todoTitle = todo.querySelector("h3").innerHTML.toLowerCase();
+
+        const toLowerSearch = search.toLowerCase();
+        
+        todo.style.display = "flex";
+
+        if (!todoTitle.includes(toLowerSearch)) {
+            todo.style.display = "none";
+        }
+
+    })
+
+};
+
+const updateTodo = (text) => {
+    
+    const todo = document.querySelectorAll(".todo");
+
+    todo.forEach((todo) => {
+
+        let todoTitle = todo.querySelector("h3")
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
+    })
+}
+
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const inputValue = todoInput.value;
 
-    if (!inputValue) {
+    if (inputValue) {
         saveTodo(inputValue);
     }
+
+});
+
+document.addEventListener("click", (e) => {
+    const targetElement = e.target;
+    const parentElement = targetElement.closest("div");
+    let todoTitle;
+
+    if (parentElement && parentElement.querySelector("h3")) {
+        todoTitle = parentElement.querySelector("h3").innerText;
+    }
+
+    if (targetElement.classList.contains("finish-todo")) {
+        parentElement.classList.toggle("done");
+    }
+
+    if (targetElement.classList.contains("remove-todo")) {
+        parentElement.remove();
+    }
+
+    if (targetElement.classList.contains("edit-todo")) {
+        toggleForms();
+
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
+    }
 })
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    toggleForms();
+})
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+
+    if (editInputValue) {
+        updateTodo(editInputValue);
+    }
+
+    toggleForms();
+})
+
+searchInput.addEventListener("keyup", (e) => {
+
+    const search = e.target.value;
+
+    getSearchTodos(search);
+})
+
+eraseBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    searchInput.value = "";
+
+    searchInput.dispatchEvent(new Event("keyup"));
+});
