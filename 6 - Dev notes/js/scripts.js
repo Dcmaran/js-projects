c1dadf69d6c8b43537accd0ec58f882137abb747
Dcmaran <diogocmr2002@gmail.com>
2023-10-31 22:04:21 -0300

    .

--- 6 - Dev notes/js/scripts.js
+++ 6 - Dev notes/js/scripts.js
@@ -4,8 +4,22 @@ const noteInput = document.querySelector("#note-content");
 
 const addNoteBtn = document.querySelector(".add-note");
 
+
+function showNotes(){
+    cleanNotes();
+    getNotes().forEach((note) => {
+        const noteElement = createNote(note.id, note.content,note.fixed);
+
+        notesContainer.appendChild(noteElement);
+    });
+}
+
+function cleanNotes(){
+    notesContainer.replaceChildren([]);
+}
+
 function addNote(){
-    
+    const notes = getNotes();
     const noteObject = {
         id: generateId(),
         content: noteInput.value,
@@ -15,6 +29,25 @@ function addNote(){
     const noteElement = createNote(noteObject.id, noteObject.content);
 
     notesContainer.appendChild(noteElement);
+
+    notes.push(noteObject);
+
+    saveNotes(notes)
+
+    noteInput.value = "";
+}
+
+function saveNotes(notes) {
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+function getNotes() {
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+
+    const orderedNotes = notes.sort((a, b) => (a.fixed > b.fixed ? -1 : 1));
+;
+
+    return orderedNotes;
 }
 
 function createNote(id, content, fixed){
@@ -31,7 +64,34 @@ function createNote(id, content, fixed){
 
     element.appendChild(textArea);
 
+    if(fixed){
+        element.classList.add("fixed");
+    }
+
+    const pinIcon = document.createElement("i")
+
+    pinIcon.classList.add(...["bi", "bi-pin"]);
+
+    element.appendChild(pinIcon);
+
+    element.querySelector(".bi-pin").addEventListener("click", () => {
+        toggleFixNote(id);
+    });
+
     return element;
+}
+
+function toggleFixNote(id){
+    const notes = getNotes();
+
+    const targetNotes = notes.filter((note) => note.id === id
+    )[0];
+
+    targetNotes.fixed = !targetNotes.fixed;
+
+    saveNotes(notes);
+
+    showNotes();
 }
 
 function generateId() {
@@ -41,3 +101,4 @@ function generateId() {
 
 addNoteBtn.addEventListener("click", () => addNote())
 
+showNotes();
