704d9522f20f35af00d3f873c61cc2de20f64ef4
Dcmaran <diogocmr2002@gmail.com>
2023-11-07 19:11:26 -0300

    .

--- 7 - Box Shadow Generator/js/scripts.js
+++ 7 - Box Shadow Generator/js/scripts.js
@@ -27,6 +27,31 @@ class BoxShadowGenerator {
             this.webkitRule = webkitRule;
             this.mozRule = mozRule;
     }
+
+    initialize () {
+
+        this.horizontalRef.value = this.horizontal.value
+        this.verticalRef.value = this.vertical.value
+        this.spreadRef.value = this.spread.value
+        this.blurRef.value = this.blur.value
+        this.applyRule();
+        this.showRule();
+    }
+
+    applyRule() {
+
+        this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px  ${this.blurRef.value}px  ${this.spreadRef.value}px #000`;
+
+        this.currentRule = this.previewBox.style.boxShadow;
+    }
+
+    showRule(){
+
+        this.rule.innerText = this.currentRule;
+        this.webkitRule.innerText = this.currentRule;
+        this.mozRule.innerText = this.currentRule;
+
+    }
 }
 
 const horizontal = document.querySelector("#horizontal")
@@ -34,7 +59,7 @@ const horizontalRef = document.querySelector("#horizontal-value")
 const vertical = document.querySelector("#vertical")
 const verticalRef = document.querySelector("#vertical-value")
 const blur = document.querySelector("#blur")
-const blurRef = document.querySelector("#blur-ref")
+const blurRef = document.querySelector("#blur-value")
 const spread = document.querySelector("#spread")
 const spreadRef = document.querySelector("#spread-value")
 
@@ -43,3 +68,19 @@ const previewBox = document.querySelector("#box")
 const rule = document.querySelector("#rule span")
 const webkitRule = document.querySelector("#webkit-rule span")
 const mozRule = document.querySelector("#moz-rule span")
+
+const boxShadow = new BoxShadowGenerator(horizontal, 
+    horizontalRef, 
+    vertical, 
+    verticalRef,
+    blur,
+    blurRef,
+    spread,
+    spreadRef,
+    previewBox,
+    rule,
+    webkitRule,
+    mozRule);
+
+
+    boxShadow.initialize();
